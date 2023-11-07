223b4f838938e8d09863dd3e97d8a839b0be1333
lucamodica <luca.modica55@gmail.com>
2023-11-07 11:41:14 +0100

    🦄 refactor(assignment1)

--- test/test.r
+++ test/test.r
@@ -1,19 +1,7 @@
-# Load the dataset (you can replace 'mtcars' with your own dataset)
-data <- mtcars
+library(ggplot2) #Only need to do once
 
-# Calculate the average and standard deviation of all numeric columns
-averages <- colMeans(data)
-std_devs <- apply(data, 2, sd)
+# exercise 1.14
+xvals <- seq(-4,6,.02)
+plotdata <- data.frame(x = xvals, y = dnorm(xvals, 1, 2))
+ggplot(plotdata, aes(x = x, y = y)) + geom_line()
 
-# Brief description of the dataset
-description <- paste("This dataset contains information about various car models. It includes columns such as mpg (miles per gallon), cyl (number of cylinders), hp (horsepower), and more.")
-
-# Print the results
-cat("Average values for each column:\n")
-print(averages)
-
-cat("\nStandard deviations for each column:\n")
-print(std_devs)
-
-cat("\nDataset Description:\n")
-cat(description)
