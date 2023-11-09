ccd20442ea502817ce0e212b58e31298ffc605bb
Hugo Silva <hugo.hes.silva@gmail.com>
2023-11-10 00:28:21 +0100

    feat: correct code for 1.e

--- assignment1/1/e.R
+++ assignment1/1/e.R
@@ -1,46 +1,32 @@
 library(ggplot2)
 
-# Observed failure times
+# Data and parameters
 data <- c(0.66, 2.30, 1.98, 1.49, 0.62)
+kappa <- 2  # shape parameter
+a <- 0      # lower bound for uniform prior
+b <- 5      # upper bound for uniform prior
+theta_values <- seq(a, b, length.out = 1000)  # Discretize theta space
 
-# Weibull log-likelihood function
-log_likelihood_weibull <- function(x, theta) {
-  kappa <- 2 # shape parameter
-  # Prevent taking sqrt of negative number
-  if (theta <= 0) {
-    return(-Inf)
-  }
-  lambda <- sqrt(theta) # scale parameter as the square root of theta
-  # Weibull log-likelihood for a vector of x and single theta
-  log_likelihood <- log(kappa) - log(lambda) + (kappa - 1) * log(x) - (kappa - 1) * log(lambda) - (x / lambda)^kappa
-  # Sum of log-likelihoods for all x due to independence
-  sum(log_likelihood)
+# Uniform prior
+prior <- rep(1 / (b - a), length(theta_values))
+
+# Weibull likelihood function
+likelihood_weibull <- function(x, theta) {
+  if (theta <= 0) return(0)
+  lambda <- sqrt(theta)  # Scale parameter
+  (kappa / lambda) * (x / lambda)^(kappa - 1) * exp(-(x / lambda)^kappa)
 }
 
-# Prior parameters for Unif(0, 5)
-a <- 0
-b <- 5
-prior_unif <- function(theta) {
-  ifelse(theta >= a & theta <= b, log(1 / (b - a)), -Inf) # log Uniform density function
-}
+# Compute the posterior for each theta
+posterior <- sapply(theta_values, function(theta) {
+  prod(sapply(data, likelihood_weibull, theta = theta))
+}) * prior
 
-# Discretize the parameter space for theta
-theta_values <- seq(a, b, length.out = 1000) # 1000 points from 0 to 5
-
-# Compute the unnormalized log-posterior for each theta value
-log_posterior_unnormalized <- sapply(theta_values, function(theta) {
-  log_likelihood_weibull(data, theta) + prior_unif(theta)
-})
-
-# Convert log-posterior to actual values avoiding underflow
-max_log_posterior <- max(log_posterior_unnormalized, na.rm = TRUE)
-posterior_unnormalized <- exp(log_posterior_unnormalized - max_log_posterior)
-
-# Normalize the posterior so it sums to 1
-posterior_normalized <- posterior_unnormalized / sum(posterior_unnormalized)
+# Normalize the posterior
+posterior <- posterior / sum(posterior * diff(theta_values[1:2]))
 
 # Create a data frame for plotting
-posterior_df <- data.frame(theta = theta_values, posterior = posterior_normalized)
+posterior_df <- data.frame(theta = theta_values, posterior = posterior)
 
 # Plot the posterior
 ggplot(posterior_df, aes(x = theta, y = posterior)) +
