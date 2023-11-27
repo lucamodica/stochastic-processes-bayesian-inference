5878f48c3416d84b08eebc203ed8d8afdc1df2b0
lucamodica <luca.modica55@gmail.com>
2023-11-28 00:54:00 +0100

    ✨ feat(assignment2): 2b done

--- assignment2/2.R
+++ assignment2/2.R
@@ -1,9 +1,22 @@
+# function to simulate a branching process 
+# with Poisson offspring distribution
+branch <- function(n, lam) { 
+	z <- c(1,rep(0,n))
+	for (i in 2:(n+1)) {
+		z[i] <- sum(rpois(z[i-1],lam));
+	}
+	return(z);
+}
+
+
 # question 2b
-extinction_prob <- function(lambda) {
-  func <- function(x) { x - exp(lambda * (x - 1)) }
+extinction_prob <- function(lam) {
+  pgf <- function(s) { exp(lam * (s - 1)) }
   
   # Use optimize the minimize the function
-  prob <- optimize(func, c(0, 1))$minimum;
+  prob <- optimize(pgf, c(0, 1))$minimum;
   
   return(prob);
 }
+
+print(extinction_prob(3));
