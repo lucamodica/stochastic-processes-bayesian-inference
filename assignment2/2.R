4efc906fbc1b0b3c856dd0677e67cef33322a2d7
lucamodica <luca.modica55@gmail.com>
2023-11-28 10:27:15 +0100

    🎈 perf(assignment 2): move posterior_lambda and the specific case of the ex into 2 separate funcions

--- assignment2/2.R
+++ assignment2/2.R
@@ -8,15 +8,25 @@ branch <- function(n, lam) {
 	return(z);
 }
 
+# posterior for lambda, derived from question 2a.
+posterior_lambda <- function(lam, data) {
+  return (dgamma(lam, sum(data), length(data)));
+}
 
-# question 2b
+# computed posterior for the observed data
+# (Z0, Z1, Z2, Z3, Z4) = (1, 1, 2, 5, 7)
+posterior_Z0_Z4 <- function(lam) {
+  return (posterior_lambda(lam, c(1, 1, 2, 5, 7)));
+};
+
+# question 2b: the extintion probability
 extinction_prob <- function(lam) {
   pgf <- function(s) { exp(lam * (s - 1)) }
   
-  # Use optimize the minimize the function
+  # Use optimize the minimize the function (the extinction
+  # probability is the smallest fixed point of the pgf)
   prob <- optimize(pgf, c(0, 1))$minimum;
   
   return(prob);
 }
 
-print(extinction_prob(3));
